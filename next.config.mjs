57d5d24a59879f2ead75f84ece9af7dc19d55ce9
GaneshManda <g.balaraju@steinbeis-next.de>
2025-03-22 15:04:05 +0100

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,21 +5,24 @@ const nextConfig = {
   assetPrefix: '/portfolio',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "ext.same-assets.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
+        pathname: "/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
